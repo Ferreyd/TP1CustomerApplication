4e78baa23a6d10089eeb7e23105e9730735fd281
Nicolas Cordina <cordina.nicolas@gmail.com>
2014-10-02 15:05:18 +0200

    Ajout des dossiers du projet

---
+++ TP1CustomerApplication-ejb/src/java/session/CustomerManager.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import entities.Customer;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author nicolas
+ */
+@Stateless
+@LocalBean
+public class CustomerManager {
+    @PersistenceContext(unitName = "TP1CustomerApplication-ejbPU")
+    private EntityManager em;
+
+    public List<Customer> getAllCustomers() {
+        Query query = em.createNamedQuery("Customer.findAlL");
+        return query.getResultList();
+    }
+
+    public Customer update(Customer customer) {
+        return em.merge(customer);
+    }
+
+    public void persist(Object object) {
+        em.persist(object);
+    }
+   
+    
+    
+    
+}
